adcf97fdfb56bb64bad11f50fe5b8f7bb3c25f6e
TWBestATM <as5408as@gmail.com>
2017-08-19 16:37:24 +0800

    新增血條功能

---
+++ RubberBattle/Assets/Script/HPUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HPUI : MonoBehaviour {
+    [SerializeField]
+    private Image m_Image;
+
+	// Use this for initialization
+	void Start () {
+	}
+    /// <summary>
+    /// 跟新血量
+    /// </summary>
+    /// <param name="percent">血量百分比</param>
+    public void UpdateHPUI(float percent)
+    {
+        m_Image.fillAmount = percent;
+    }
+}
