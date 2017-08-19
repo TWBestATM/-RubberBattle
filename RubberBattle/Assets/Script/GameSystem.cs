7bb314fc19d8617e93291eeb528441aa3d4f2ea9
TWBestATM <as5408as@gmail.com>
2017-08-19 15:20:03 +0800

    新增時間倒數

--- RubberBattle/Assets/Script/GameSystem.cs
+++ RubberBattle/Assets/Script/GameSystem.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameSystem : MonoBehaviour {
+    /// <summary>
+    /// 一回合時間
+    /// </summary>
+    [SerializeField]
+    private float BattleTime =60;
 
-	// Use this for initialization
-	void Start () {
-		
+    enum Satue {
+        Begin,
+        Battle,
+        End
+    }
+    public  static GameSystem Instance;
+    public float GameTime = 60;
+    private Satue GameSatue;
+    // Use this for initialization
+    private void Awake()
+    {
+         Instance = this;      
+    }
+    void Start () {
+ 
+        GameSatue = Satue.Begin;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        switch (GameSatue)
+        {
+            case Satue.Begin:
+                GameTime = BattleTime;
+                GameSatue = Satue.Battle;
+                break;
+            case Satue.Battle:
+                //數字倒數
+                GameTime -= Time.deltaTime;
+                if (GameTime <= 0)
+                {
+                    GameSatue =Satue.End;
+                }
+                break;
+            case Satue.End:
+                break;
+        }
 		
 	}
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
 }
---
+++ RubberBattle/Assets/Script/TimeUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimeUI : MonoBehaviour {
+
+
+
+    private Text ShowText;
+    private GameSystem m_GameSystem;
+	// Use this for initialization
+	void Start () {
+        ShowText = GetComponent<Text>();
+        m_GameSystem = GameSystem.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //數字Floor
+        ShowText.text = (Mathf.FloorToInt(m_GameSystem.GameTime)).ToString();
+	}
+}
