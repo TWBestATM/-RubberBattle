14c08fd857772064b5d7f0f21d8b9cd1710be0d5
TWBestATM <as5408as@gmail.com>
2017-08-20 11:23:13 +0800

    開場播放控制

---
+++ RubberBattle/Assets/Script/BackGroundCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundCtrl : MonoBehaviour {
+
+
+    private Animator m_Animator;
+    [SerializeField]
+    private GameObject UI;
+	// Use this for initialization
+	void Start () {
+        UI.SetActive(false);
+        GameSystem.Instance.GameStart += StartGame;
+        m_Animator = GetComponent<Animator>();
+	}
+    void opening1End()
+    {
+        GameSystem.Instance.OpeningEnd();
+
+    }
+    void StartGame()
+    {
+        m_Animator.SetTrigger("Start");
+    }
+    void ToSelect()
+    {
+        UI.SetActive(true);
+
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
